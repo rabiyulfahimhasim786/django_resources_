25410d907e9ff26bbe8bb03974a579c2ac645512
Rabiyulfahim <76527084+rabiyulfahimhasim786@users.noreply.github.com>
2022-06-15 20:49:26 +0530

    Add files via upload

---
+++ views-basics/django-images/mysite/polls/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+# posts/models.py
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.TextField()
+    cover = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
---
+++ views-basics/django-images/mysite/polls/urls.py
@@ -0,0 +1,11 @@
+# posts/urls.py
+from django.urls import path
+
+#from .views import HomePageView
+from .views import HomePageView, CreatePostView # new
+
+
+urlpatterns = [
+    path("", HomePageView.as_view(), name="home"),
+    path("post/", CreatePostView.as_view(), name="add_post"),
+]
