30bcae0242773b36b7152de7bf997a73c28aa313
Rabiyulfahim <76527084+rabiyulfahimhasim786@users.noreply.github.com>
2022-08-25 06:24:46 -0500

    Add files via upload

--- gsearch form data/polls/urls.py
+++ gsearch form data/polls/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.home, name='home'),
     path('get/', views.getting, name='getting'),
-    path('var/', views.variable, name='variable')
+    path('var/', views.variable, name='variable'),
+    path('csv/', views.csv, name='csv'),
+    path('form/', views.excel, name='excel'),
+    path('datatable/', views.datatable, name='datatable')
 ]
--- gsearch form data/polls/views.py
+++ gsearch form data/polls/views.py
@@ -17,7 +17,17 @@ from .forms import SubscribeForm
 def index(request):
     return HttpResponse("Hello, world!")
 
+import pandas as pd
+import sqlite3
+from sqlite3 import Error
 
+def csv(request):
+    db_file = './db.sqlite3'
+    conn = sqlite3.connect(db_file, isolation_level=None,
+                       detect_types=sqlite3.PARSE_COLNAMES)
+    db_df = pd.read_sql_query("SELECT * FROM polls_person", conn)
+    db_df.to_csv('polls/static/data/database.csv', index=False)
+    return HttpResponse("Hello, world!")
 
 def getting(request):
     books = Person.objects.all()
@@ -84,3 +94,54 @@ def variable(request):
     print(rank)
     return render(request, 'i.html', {'testvar': testvar, 'rank': rank})
 
+
+def excel(request):
+  # if this is a POST request we need to process the form data
+  if request.method == 'POST':
+    key = request.POST.get('name')
+    webs = request.POST.get('web')
+    #print(title)
+    #print(website)
+    try:
+        from googlesearch import search
+    except ImportError:
+        print("No module named 'google' found")
+    #website = # need a exact site name including /
+    # to search
+    query = key#"geeksforgeeks" # keyword
+    #rank, rank_list = 1, ""
+    squares = []
+    for j in search(query, tld="co.in", num=10, stop=10, pause=2):
+        #print(j)
+        squares.append(j)
+    #print(squares)
+# app.py
+
+#streaming = ['netflix', 'hulu', 'disney+', 'appletv+']
+    streaming = squares
+    index = streaming.index(webs)
+    ranking = index+1
+    rankings = "Google rank of your site is:"+str(ranking) #index+1)
+    #form = MyForm(request.POST)
+    # create a form instance and populate it with data from the request:
+    form = SubscribeForm(request.POST)
+    # check whether it's valid:
+    if form.is_valid():
+        name = form.cleaned_data['name']
+        web = form.cleaned_data['web']
+        p = Person(name=name, web=web, output=ranking, date_subscribed=datetime.now())
+        p.save()
+        # process the data in form.cleaned_data as required
+        
+        # redirect to a new URL:
+        return render(request, 'csv.html', {'form': form})
+  # if a GET (or any other method) we'll create a blank form    
+  else: 
+    form = SubscribeForm()
+
+  return render(request, 'csv.html', {'form': form})
+
+def datatable(request):
+    file = 'polls/static/data/database.csv'
+    df = pd.read_csv(file)
+    return render(request, 'datatable.html', {'columns': df.columns, 'rows': df.to_dict('records')})
